fc6783f2f57d556e19c4bb572baae227f973b353
daniel lissner <dlissner@gmail.com>
2013-12-25 00:00:56 -0600

    Fix user and user_event create migrations

--- db/migrate/20131215013736_create_user_events.rb
+++ db/migrate/20131215013736_create_user_events.rb
@@ -1,6 +1,10 @@
 class CreateUserEvents < ActiveRecord::Migration
   def change
     create_table :user_events do |t|
+      t.belongs_to  :user
+      t.belongs_to  :event
+      
+      t.timestamps
     end
   end
 end
